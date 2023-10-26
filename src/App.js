ca26c95bea5f3d1bbf896e7f648a274e4251bc23
ArtSatanov <artsatanov@gmail.com>
2023-10-27 02:42:59 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Products from './components/Products/Products';
+import NotFound from './components/NotFound/NotFound';
+
 export const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
