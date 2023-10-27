9f476a6a1e2fed9ec7d791f94462abba1ac5906a
ArtSatanov <artsatanov@gmail.com>
2023-10-27 22:24:59 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Products from './components/Products/Products';
 import NotFound from './components/NotFound/NotFound';
 
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
     <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
