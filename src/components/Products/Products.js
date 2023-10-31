84131ac8c7e63232a7a90d1e400e07a6d75b097f
ArtSatanov <artsatanov@gmail.com>
2023-10-31 02:54:18 +0200

    1

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,1 +1,10 @@
-export const Products = () => {};
+import { getProducts } from '../../fakeAPI';
+
+export const Products = () => {
+  const products = getProducts();
+  return (
+    <main>
+      <ProductList products={products} />
+    </main>
+  );
+};
