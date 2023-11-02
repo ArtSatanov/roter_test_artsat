6eae7f47549d21b054823c273271327d4b57e195
ArtSatanov <artsatanov@gmail.com>
2023-11-02 19:20:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/roter_test_artsat">
       <App />
     </BrowserRouter>
   </React.StrictMode>
